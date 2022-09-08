c21ff2aecaed2f4ee04b60d05ec9d2be0898a07f
procfs <woosun.song@dream-academy.io>
2022-09-08 14:44:09 +0900

    implement pyramid string generation

--- src/Pyramid.sol
+++ src/Pyramid.sol
@@ -4,14 +4,12 @@ pragma solidity ^0.8.13;
 contract Pyramid {
     function run(uint size) pure public returns (string memory _out) {
         _out = ""; // TODO
-        /* Example (size = 5):
-         *
-         *     "*\n"
-         *     "**\n"
-         *     "***\n"
-         *     "****\n"
-         *     "*****\n"
-         *
-         */
+        for (uint i = 0; i < size; i++) {
+            for (uint j = 0; j <= i; j++) {
+                _out = string.concat(_out, "*");
+            }
+            _out = string.concat(_out, "\n");
+        }
+        return _out;
     }
 }
